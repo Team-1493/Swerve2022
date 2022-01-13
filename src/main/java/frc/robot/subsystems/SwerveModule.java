45bdfde2931e5b7479280a18e2a3c87a68571dc8
Endri Hoxha <66202eho0@albany.k12.ny.us>
2022-01-13 18:22:05 -0500

    defined TalonFX motors

--- src/main/java/frc/robot/subsystems/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -1,13 +1,21 @@
 package frc.robot.subsystems;
 
-
-
+import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
 
 public class SwerveModule{
 
     
 public SwerveModule(){
-    
+    TalonFX motor1 = new TalonFX(1);
+    TalonFX motor2 = new TalonFX(2);
+    TalonFX motor3 = new TalonFX(3);
+    TalonFX motor4 = new TalonFX(4);
+    TalonFX motor5 = new TalonFX(5);
+    TalonFX motor6 = new TalonFX(6);
+    TalonFX motor7 = new TalonFX(7);
+    TalonFX motor8 = new TalonFX(8);
+
 }
 
 }
