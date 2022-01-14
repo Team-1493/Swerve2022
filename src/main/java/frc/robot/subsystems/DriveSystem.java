f40b3069ced4006c4191497ed9f3bc02ee7669f4
Endri Hoxha <66202eho0@albany.k12.ny.us>
2022-01-13 21:18:44 -0500

    defined motors

--- src/main/java/frc/robot/subsystems/DriveSystem.java
+++ src/main/java/frc/robot/subsystems/DriveSystem.java
@@ -4,7 +4,10 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Devices.Stick;
 
 public class DriveSystem extends SubsystemBase{
-    SwerveModule module = new SwerveModule();
+    SwerveModule module1 = new SwerveModule(1,2);
+    SwerveModule module2 = new SwerveModule(3,4);
+    SwerveModule module3 = new SwerveModule(5,6);
+    SwerveModule module4 = new SwerveModule(7,8);
 
     public DriveSystem(Stick joystick){
 
