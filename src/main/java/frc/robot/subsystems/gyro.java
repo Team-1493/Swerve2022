4ea50f8d03adfa5c5677062625b55f6645d0b25b
Saugat Shah <88122ssh@albany.k12.ny.us>
2022-01-16 17:42:04 -0500

    Upd2Gyro

--- src/main/java/frc/robot/subsystems/gyro.java
+++ src/main/java/frc/robot/subsystems/gyro.java
@@ -8,7 +8,7 @@ import edu.wpi.first.math.geometry.Rotation2d;
 //creates instance of pigeon
 public class gyro
 {
-    public double gyro() 
+    private PigeonIMU pigeon = new PigeonIMU(20);
     {
         //resets to default values
         PigeonIMU pigeon = new PigeonIMU(20);
@@ -19,19 +19,26 @@ public class gyro
         pigeon.getYawPitchRoll(ypr_deg);
                 double yawAngle=ypr_deg[0];
         //gets direction of robot (-180 - 180)
+        public class angles{
         int ang;
         double outputval;
         double inputval;
         ang=360;
         inputval=yawAngle;
         outputval= inputval % ang;
-        double absAng= outputval - 180;
-        return absAng;
+        if (outputval<=0) {
+            double absAng= outputval + 180; 
+                        }
+        else {
+            double absAng= outputval - 180; 
+                            
+        return absAng; 
         //returns yaw angle in degrees (0-360)
         return outputval;
         //gets gyro temp
+        public class getTemp{
             double temp=pigeon.getTemp();
-            return temp;
+            return temp;}
         //Returns yaw angle in radians
         double absAngRad = (absAng*3.14)/180;
         return absAngRad;
@@ -40,6 +47,8 @@ public class gyro
                 return new Rotation2d(getAngleRadians());
         //resets the yaw angle to zero 
         resetGyro(pigeon);
+                    }
+                    }
     }
 
     private void resetGyro(PigeonIMU pigeon) {
