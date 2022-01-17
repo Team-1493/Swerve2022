3b634a9762a29018b448c00a1f94eae8d3e7d336
Trent Minarich <20014tmi0@albany.k12.ny.us>
2022-01-17 18:08:11 -0500

    Created shooter class and defined the motor

--- src/main/java/frc/robot/subsystems/shooter.java
+++ src/main/java/frc/robot/subsystems/shooter.java
@@ -1,10 +1,13 @@
 package frc.robot.subsystems;
  
-//    import com.ctre.phoenix.motorcontrol.can.TalonFX;
+    import com.ctre.phoenix.motorcontrol.can.TalonFX;
  
 public class shooter{
     
- 
+    public shooter(int shooterPort){
+        TalonFX ShooterMotor = new TalonFX(shooterPort);
+    }
+
 
    
-}
+}  
