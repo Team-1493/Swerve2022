9fb902ffc8d44f049038095cbaefaac692464e0c
Team-1493 <acastagna@albany.k12.ny.us>
2022-01-20 15:56:38 -0500

    commit

--- src/main/java/frc/robot/subsystems/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -11,16 +11,16 @@ public class SwerveModule{
     public TalonFX drive;
     public CANCoder turnCoder;
     public CANCoder driveCoder;
-    public SwerveModule(int driveio, int turnio, int drivecoderio, int turncoderio){
+    public SwerveModule(int driveio, int turnio,  int turncoderio){
         turn = new TalonFX(driveio);
         drive = new TalonFX(turnio);
-        driveCoder = new CANCoder(drivecoderio);
         turnCoder = new CANCoder(turncoderio);
 
         turn.configRemoteFeedbackFilter(turnCoder, 0,25);
         turn.configSelectedFeedbackSensor(TalonFXFeedbackDevice.RemoteSensor0,0,25);
-        drive.configRemoteFeedbackFilter(driveCoder, 0,25);
-        drive.configSelectedFeedbackSensor(TalonFXFeedbackDevice.RemoteSensor0,0,25);
+
+        drive.configSelectedFeedbackSensor(TalonFXFeedbackDevice.IntegratedSensor,0,25);
+
 
     }
 
