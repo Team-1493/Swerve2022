dab4a88be691ee28dd1d6adaaaf1533638beee23
Team-1493 <acastagna@albany.k12.ny.us>
2022-02-10 16:36:37 -0500

    Wrote code to sync intake. Made a digital input to detect if there is a ball in the lower or upper position

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -2,47 +2,55 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
+
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Joystick;
 
 
 import frc.robot.Devices.Stick;
 
 public class Intake{
-    TalonFX BottomIntakeMotor = new TalonFX(0);
-    TalonFX MiddleIntakeMotor = new TalonFX(1);
+    TalonFX LowerPositionMotor = new TalonFX(0);
+    TalonFX UpperPositionMotor = new TalonFX(1);
+    TalonFX IntakeMotor = new TalonFX(2);
     Joystick mystick = new Joystick(0);
-
+    DigitalInput LowerPositionInput = new DigitalInput(0);
+    DigitalInput UpperPositionInput = new DigitalInput(1);
 
     public Intake(){
     
         // Factory default
-        BottomIntakeMotor.configFactoryDefault();
-        MiddleIntakeMotor.configFactoryDefault();
+        LowerPositionMotor.configFactoryDefault();
+        UpperPositionMotor.configFactoryDefault();
+        IntakeMotor.configFactoryDefault();
         
         // Inverted - False
-        BottomIntakeMotor.setInverted(false);
-        MiddleIntakeMotor.setInverted(false);
+        LowerPositionMotor.setInverted(true);
+        UpperPositionMotor.setInverted(true);
+        IntakeMotor.setInverted(true);
     
         // Brake mode
-        BottomIntakeMotor.setNeutralMode(NeutralMode.Brake);
-        MiddleIntakeMotor.setNeutralMode(NeutralMode.Brake);
+        LowerPositionMotor.setNeutralMode(NeutralMode.Brake);
+        UpperPositionMotor.setNeutralMode(NeutralMode.Brake);
+        IntakeMotor.setNeutralMode(NeutralMode.Brake);
     
         // Peak output both forward and reverse in percentages
         // Forward
-        BottomIntakeMotor.configPeakOutputForward(1);
-        MiddleIntakeMotor.configPeakOutputForward(1);
+        LowerPositionMotor.configPeakOutputForward(1);
+        UpperPositionMotor.configPeakOutputForward(1);
+        IntakeMotor.configPeakOutputForward(1);
         // Reverse
-        BottomIntakeMotor.configPeakOutputReverse(-1);
-        MiddleIntakeMotor.configPeakOutputReverse(-1);
+        LowerPositionMotor.configPeakOutputReverse(-1);
+        UpperPositionMotor.configPeakOutputReverse(-1);
+        IntakeMotor.configPeakOutputReverse(-1);
     
         // Voltage Saturation
-        BottomIntakeMotor.configVoltageCompSaturation(10);
-        MiddleIntakeMotor.configVoltageCompSaturation(10);
+        LowerPositionMotor.configVoltageCompSaturation(10);
+        UpperPositionMotor.configVoltageCompSaturation(10);
+        IntakeMotor.configVoltageCompSaturation(10);
     
-        // Voltage Compensation
-        BottomIntakeMotor.configVoltageCompensation(true);
-        MiddleIntakeMotor.configVoltageCompensation(true);
     
     }
 
@@ -50,13 +58,33 @@ public class Intake{
     public void IntakeSystem() {
 
         if (mystick.getRawButton(1)) {
-        BottomIntakeMotor.set(TalonFXControlMode.PercentOutput,0.5);
-        MiddleIntakeMotor.set(TalonFXControlMode.PercentOutput,0.5);
+            IntakeMotor.set(ControlMode.PercentOutput, 0.5);
+            LowerPositionMotor.set(ControlMode.PercentOutput, 0.5);
         }
-        else {
-        BottomIntakeMotor.set(TalonFXControlMode.PercentOutput,0);
-        MiddleIntakeMotor.set(TalonFXControlMode.PercentOutput,0);
+        else{
+            IntakeMotor.set(ControlMode.PercentOutput, 0);
+            LowerPositionMotor.set(ControlMode.PercentOutput, 0.5);
         }
+
+        //Check if there is a ball in the lower position
+        if (LowerPositionInput.get()) {
+            //check if there is one in the upper position
+
+            //if there is also a ball in the upper position lower position motor will stop
+            if (UpperPositionInput.get()) {
+            LowerPositionMotor.set(ControlMode.PercentOutput, 0);
+            }
+            
+            //if there is not a ball in the upper position run the motors until one makles it up there
+            else {
+                while (UpperPositionInput.get() == false)
+                    LowerPositionMotor.set(ControlMode.PercentOutput, 0.5);
+                    UpperPositionMotor.set(ControlMode.PercentOutput, 0.5);
+            }
+                
+        }
+        
+
 }
 
 }
