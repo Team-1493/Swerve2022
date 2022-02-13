9b1cce82c83dcf60000aa1d2e5a83aa3d1cc24d5
Team-1493 <acastagna@albany.k12.ny.us>
2022-02-12 20:44:10 -0500

    Update gyro.java

--- src/main/java/frc/robot/subsystems/gyro.java
+++ src/main/java/frc/robot/subsystems/gyro.java
@@ -1,39 +1,29 @@
-package frc.robot.subsystems;
+package frc.robot.Sensors;
 /**
  * @author Saugat
  */
 
 import java.lang.reflect.Method;
-
-//imports pigeon
 import com.ctre.phoenix.sensors.PigeonIMU;
-//imports Rotation2d
 import edu.wpi.first.math.geometry.Rotation2d;
-//creates instance of pigeon
-public class gyro
+
+public class test
 {
    private PigeonIMU pigeon = new PigeonIMU(20);
     {
         //resets to default values
         PigeonIMU pigeon = new PigeonIMU(20);
-        
         pigeon.configFactoryDefault();
         pigeon.setYaw(0);
-        
-        //gets the yaw angle
-        double[] ypr_deg = new double[3];
-        
-        pigeon.getYawPitchRoll(ypr_deg);
-        double yawAngle = ypr_deg[0];
-          
     }
+   
     //returns 2d object
     public Rotation2d getRotation2d(){
     return new Rotation2d(getAngleRadians()); 
     }
+   
     //resets yaw angle to zero
-    resetGyro(pigeon);
-    private void resetGyro(PigeonIMU pigeon) {
+    private void resetGyro() {
         pigeon.configFactoryDefault();
         pigeon.setYaw(0);
     }
@@ -44,32 +34,21 @@ public class gyro
         double[] ypr_deg = new double[3];
         pigeon.getYawPitchRoll(ypr_deg);
         double yawAngle = ypr_deg[0]; 
-        int ang;
-        double outputval;
-        double inputval;
-        double absAng;
-        ang = 360;
-        
-        inputval = yawAngle;
-        outputval = inputval % ang;
-        if (outputval <= 0) {
-            absAng= outputval + 180; 
-        }
-        else {
-            absAng = outputval - 180; 
-        }
-        return absAng;
+        if (yawAngle > 180) yawAngle=yawAngle-360;
+        else if (yawAngle <-180) yawAngle = yawAngle + 360;
+        return yawAngle;
     }
-        //gets gyro temp
+
+//gets gyro temp
     public double getTemp(){
-            double temp=pigeon.getTemp();
-            return temp; 
-                                }
+        double temp=pigeon.getTemp();
+        return temp; 
+    }
     
-        //Returns yaw angle in radians
+ //Returns yaw angle in radians
     public double getAngleRadians(){
         double AngRad = (getangle()*Math.PI)/180;
-        return AngRad;
+    return AngRad;
     } 
 }
 
